054aea4311d99aa92a31e28a5802cc2666ff9155
Polina Popova <irkomix2002@gmail.com>
2025-09-21 20:16:25 +0200

    Added initial variables, Added Check for Valid Guess

--- guess_sequence/sequence.js
+++ guess_sequence/sequence.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// document.querySelector('body').style.backgroundColor = '#aaa';
+// VARIABLES
+const sequenceLength = 4;
+const answerEls = document.querySelectorAll('.sequence-num');
+const messageEl = document.querySelector('.message');
+let answer = '';
+let guess;
+
+function displayErrorMessage() {
+  messageEl.classList.remove('hidden');
+}
+
+for (let i = 0; i < sequenceLength; i++) {
+  answer += Math.trunc(Math.random() * 10);
+}
+
+answerEls.forEach((el, i) => {
+  el.textContent = answer[i];
+});
+
+document.querySelector('.check').addEventListener('click', function () {
+  guess = document.querySelector('.input').value;
+
+  if (guess === '' || guess.length !== 4) displayErrorMessage();
+  else messageEl.classList.add('hidden');
+
+  console.log(guess);
+});
