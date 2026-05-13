062f16193ce78e0aacadfdef5c59eff34caec34a
Polina Popova <irkomix2002@gmail.com>
2026-05-13 15:43:10 +0200

    style: game menu

--- src/pages/memory-game/MemoryCardsGame.jsx
+++ src/pages/memory-game/MemoryCardsGame.jsx
@@ -1,5 +1,52 @@
 import './MemoryCards.css';
+import { ImSpades, ImHeart, ImDiamonds, ImClubs } from 'react-icons/im';
 
 export default function MemoryCardsGame() {
-  return <div>Hiiii</div>;
+  return (
+    <section className="memory-game-menu">
+      <Timer />
+      {new Array(8).fill(null).map((_, i) => (
+        <MemoryCard key={i} />
+      ))}
+      <MovesCounter />
+    </section>
+  );
+}
+
+function Timer() {
+  return <div>Time</div>;
+}
+
+// function MemoryCard() {
+//   return <div className="memory-card">Hello</div>;
+// }
+
+function MemoryCard() {
+  return (
+    <div className="memory-card">
+      <div className="card-inner">
+        <div className="card-corner top">
+          <span>A</span>
+          <span className="suit">
+            <ImSpades />
+          </span>
+        </div>
+
+        <div className="card-center">
+          <ImSpades />
+        </div>
+
+        <div className="card-corner bottom">
+          <span>A</span>
+          <span className="suit">
+            <ImSpades />
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function MovesCounter() {
+  return <div>Moves</div>;
 }
--- src/pages/memory-game/MemoryCardsMenu.jsx
+++ src/pages/memory-game/MemoryCardsMenu.jsx
@@ -2,21 +2,35 @@ import { Link } from 'react-router-dom';
 import './MemoryCards.css';
 
 export default function MemoryCardsMenu() {
+  const title = '(Memory_cards)';
+
   return (
     <section className="memory-game-menu">
-      <h1>(Memory_cards)</h1>
-      <div className="memory-menu-links">
-        <Link to="/memory-cards/game" className="memory-menu-link">
-          Play
-        </Link>
+      <div className="menu-content">
+        <h1 className="casino-title">
+          {title.split('').map((char, index) => (
+            <span key={index} style={{ animationDelay: `${index * 0.06}s` }}>
+              {char}
+            </span>
+          ))}
+        </h1>
+        <div className="memory-menu-links">
+          <Link to="/memory-cards/game" className="memory-menu-link">
+            Play
+          </Link>
 
-        <div className="memory-menu-link">
-          <p>Easy</p>
+          <div className="memory-menu-link">
+            <p>Easy</p>
+          </div>
+
+          <div className="memory-menu-link">
+            <p>Rules</p>
+          </div>
+
+          <Link to="/" className="memory-menu-link">
+            Quit
+          </Link>
         </div>
-
-        <Link to="/" className="memory-menu-link">
-          Quit
-        </Link>
       </div>
     </section>
   );
